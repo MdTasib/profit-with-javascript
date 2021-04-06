814a3f33e93503cfed0a91959dbe5d2a00585c66
OhidulAlam <mdtasib442@gmail.com>
2021-04-06 17:29:54 +0600

    Data types

---
+++ lessons/lesson18.js
@@ -0,0 +1,8 @@
+// Data types
+let myAge = 19; // number
+let myName = 'Tasib' // string
+let fullName = { firstName: 'Mohammad', lastName: 'Tasib' }; // object
+let truth = false / true; // boolean
+let skills = ['HTML', 'CSS', 'Bootstrap', 'CMS', 'JavaScript', 'React']; // array
+let random; // undefined
+let nothing = null; // null
