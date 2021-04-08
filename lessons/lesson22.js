dc9c6157df4a3a3e16655a42625e1d7630b3b91b
OhidulAlam <mdtasib442@gmail.com>
2021-04-08 11:59:50 +0600

    Conditionals, control flows (if else)

---
+++ lessons/lesson22.js
@@ -0,0 +1,29 @@
+// conditionals, control flows (if else)
+
+// 18-35 is my target demographic
+// && AND
+// || OR
+
+let age = prompt('What is your age?');
+
+if ((age >= 18) && (age <= 35)) {
+    console.log('Target demo');
+} else {
+    console.log('Not my audience');
+}
+
+// Switch statements
+switch (6) {
+    case 0:
+        text = 'weekend';
+        break;
+    case 5:
+        text = 'weekend';
+        break;
+    case 6:
+        text = 'weekend';
+        break;
+    default:
+        text = 'weekday';
+}
+console.log(text);
