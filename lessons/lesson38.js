51a7f3b38b77656382bf6d2a99607924b81bae4a
MdTasib <ohidulalam442@gmail.com>
2021-04-15 23:30:53 +0600

    Algorithm - first item in Array

---
+++ lessons/lesson38.js
@@ -0,0 +1,14 @@
+// first item in Array
+
+function getFirstItem(yourItem) {
+    return yourItem[0];
+}
+
+const age = [6, 4, 3, 2];
+console.log(getFirstItem(age));
+
+const name = ['Tasib', 'Rafi', 'Rakib'];
+console.log(getFirstItem(name));
+
+const me = 'Tasib';
+console.log(getFirstItem(me));
