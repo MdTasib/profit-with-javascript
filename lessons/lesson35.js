b432b000d6be454c945937de1b49554c50b840d1
MdTasib <ohidulalam442@gmail.com>
2021-04-15 22:38:11 +0600

    Algorithm addNumbers

---
+++ lessons/lesson35.js
@@ -0,0 +1,7 @@
+// Let's start Algorithms Javascript
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+console.log(addNumbers(2, 3));
+console.log(addNumbers(3, 3));
+console.log(addNumbers(4, 5));
---
+++ lessons/lesson36.js
@@ -0,0 +1,9 @@
+// Make a function -- Convert Minutes to Seconds
+
+function convertMinutesToSeconds(minutes) {
+    const seconds = 60;
+    return minutes * seconds;
+}
+
+console.log(convertMinutesToSeconds(2));
+console.log(convertMinutesToSeconds(3));
