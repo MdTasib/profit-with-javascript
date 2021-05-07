717b416ff946b00873c30925153e26434b2159ac
MdTasib <ohidulalam442@gmail.com>
2021-05-07 15:31:16 +0600

    Arrow function

---
+++ lessons/ES6-Plus/lesson75.js
@@ -0,0 +1,7 @@
+// Arrow functions
+
+const firstName = 'Mohammad';
+const lastName = 'Tasib';
+
+const getFullName = (firstName, lastName) => console.log(`${firstName} ${lastName}`);
+getFullName(firstName, lastName);
