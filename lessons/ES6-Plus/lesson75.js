dfb44a82b9d43cb7a7b2d30fa495e121a03c10e7
MdTasib <ohidulalam442@gmail.com>
2021-05-07 15:35:51 +0600

    Default parameters

--- lessons/ES6-Plus/lesson75.js
+++ lessons/ES6-Plus/lesson75.js
@@ -3,5 +3,7 @@
 const firstName = 'Mohammad';
 const lastName = 'Tasib';
 
-const getFullName = (firstName, lastName) => console.log(`${firstName} ${lastName}`);
+getFullName = (firstName, lastName) => console.log(`${firstName} ${lastName}`);
 getFullName(firstName, lastName);
+
+// Default Parameters
