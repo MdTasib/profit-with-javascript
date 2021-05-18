cacd8c46945e137e05299006f4b615b0a9e78d94
MdTasib <ohidulalam442@gmail.com>
2021-05-18 14:00:42 +0600

    Weather app

--- lessons/Weather-App/index.js
+++ lessons/Weather-App/index.js
@@ -1,24 +1,28 @@
 /**
  * Weather App
- * TODO: Complete getWeatherData() to return json response Promise
- * TODO: Complete searchCity() to get user input and get data using getWeatherData()
+ * DONE: Complete getWeatherData() to return json response Promise
+ * Done: Complete searchCity() to get user input and get data using getWeatherData()
  * TODO: Complete showWeatherData() to set the data in the the html file from response
  */
 
 // API_KEY for maps api
-let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
+let API_KEY = "ca513d9373507b97690f901a262514bc";
 
 /**
  * Retrieve weather data from openweathermap
  * HINT: Use fetch()
  * HINT: URL should look like this: 
- * https://api.openweathermap.org/data/2.5/weather?q=detroit&appid=a8e71c9932b20c4ceb0aed183e6a83bb&units=imperial
+ * https://api.openweathermap.org/data/2.5/weather?q=detroit&appid=ca513d9373507b97690f901a262514bc&units=imperial
  */
 getWeatherData = (city) => {
     const URL = "https://api.openweathermap.org/data/2.5/weather";
     //HINT: Use template literals to create a url with input and an API key
-
+    FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`
     //CODE GOES HERE
+    const weatherPromise = fetch(FULL_URL);
+    return weatherPromise.then(response => {
+        return response.json();
+    })
 }
 
 /**
@@ -28,7 +32,16 @@ getWeatherData = (city) => {
 searchCity = () => {
     const city = document.getElementById('city-input').value;
     // CODE GOES HERE
-
+    getWeatherData(city)
+        .then(response => {
+            showWeatherData(response);
+        })
+        .catch(error => {
+            let errorMsg = document.getElementById('error').innerHTML;
+            if (errorMsg === undefined) {
+                errorMsg = 'city not found'
+            }
+        })
 }
 
 /**
@@ -37,5 +50,9 @@ searchCity = () => {
  */
 showWeatherData = (weatherData) => {
     //CODE GOES HERE
-
+    document.getElementById('city-name').innerHTML = weatherData.name;
+    document.getElementById('weather-type').innerHTML = weatherData.weather[0].main;
+    document.getElementById('temp').innerHTML = weatherData.main.temp;
+    document.getElementById('min-temp').innerHTML = weatherData.main.temp_min;
+    document.getElementById('max-temp').innerHTML = weatherData.main.temp_max;
 }
