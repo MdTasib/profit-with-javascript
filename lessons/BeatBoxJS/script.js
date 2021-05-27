c3ddb2726868f5d7a8653b13e75c308c91c0af9f
MdTasib <ohidulalam442@gmail.com>
2021-05-27 20:05:46 +0600

    Button pt5: Add transition for button selection

--- lessons/BeatBoxJS/script.js
+++ lessons/BeatBoxJS/script.js
@@ -5,8 +5,8 @@
  * DONE: Button pt1: Initialize color and element values
  * DONE: Button pt2: Set button color upon initialization | Initialize button in beats["65"]
  * DONE: Button pt3: Complete select function to set the color and shadow of button upon pressing
- * TODO: Button pt4: Call the select() function upon key press ;)
- * TODO: Button pt5: Add transition for button selection
+ * DONE: Button pt4: Call the select() function upon key press ;)
+ * DONE: Button pt5: Add transition for button selection
  * TODO: Button pt6: Remove the button style upon transition end | Use deselect function
  * TODO: Complete all button instances with the following colors
  * TODO: Add background image
@@ -55,6 +55,7 @@ triggerBeat = (event) => {
     if (keyCode in beats) {
         let keyPress = beats[keyCode];
         keyPress.beat.play();
+        keyPress.button.select();
     }
 }
 
