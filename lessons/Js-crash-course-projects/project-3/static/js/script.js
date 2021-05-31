753ae15bbf92c4d1bdbf79256a1e8250419a382f
MdTasib <ohidulalam442@gmail.com>
2021-05-31 15:30:28 +0600

    project 3

--- lessons/Js-crash-course-projects/project-3/static/js/script.js
+++ lessons/Js-crash-course-projects/project-3/static/js/script.js
@@ -1,4 +1,32 @@
-function rpsGame(yourShoice) {
-    console.log(yourShoice);
-    console.log(yourShoice.src);
+function rpsGame(yourChoice) {
+    // console.log(yourChoice);
+
+    let humanChoice, botChoice;
+    // humanChoice = yourChoice.id;
+    botChoice = numberToChoice(randToRpsInt());
+    // console.log(botChoice);
+    // results = decideWinner(humanChoice, botChoice); // [0,1] human lost | bot won
+    // message = finalMessage(results); // {'message': 'You Won', 'color': 'green'}
+    // rpsFrontEnd(yourChoice.id, botChoice, message);
+}
+
+function randToRpsInt() {
+    return Math.floor(Math.random() * 3);
+}
+
+function numberToChoice(number) {
+    return ['rock', 'paper', 'scissors'][number];
+}
+
+function decideWinner(yourChoice, computerChoice) {
+    const rpsDatabase = {
+        'rock': { 'scissors': 1, 'rock': 0.5, 'paper': 0 },
+        'paper': { 'rock': 1, 'paper': 0.5, 'scissors': 0 },
+        'scissors': { 'paper': 1, 'scissors': 0.5, 'rock': 0 },
+    }
+
+    const yourScore = rpsDatabase[yourChoice][computerChoice];
+    const computerScore = rpsDatabase[computerChoice][yourChoice];
+    console.log(yourScore);
+    console.log(computerScore);
 }
