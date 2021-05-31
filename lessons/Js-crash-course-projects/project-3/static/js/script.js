5320f2cbb9f8df7cc0de00b77757736214ce76ac
MdTasib <ohidulalam442@gmail.com>
2021-05-31 19:36:48 +0600

    project 3 done

--- lessons/Js-crash-course-projects/project-3/static/js/script.js
+++ lessons/Js-crash-course-projects/project-3/static/js/script.js
@@ -1,13 +1,18 @@
 function rpsGame(yourChoice) {
-    // console.log(yourChoice);
-
     let humanChoice, botChoice;
-    // humanChoice = yourChoice.id;
+
+    humanChoice = yourChoice.id;
+
     botChoice = numberToChoice(randToRpsInt());
-    // console.log(botChoice);
-    // results = decideWinner(humanChoice, botChoice); // [0,1] human lost | bot won
-    // message = finalMessage(results); // {'message': 'You Won', 'color': 'green'}
-    // rpsFrontEnd(yourChoice.id, botChoice, message);
+    console.log('Computer Choice', botChoice);
+
+    results = decideWinner(humanChoice, botChoice); // [0,1] human lost | bot won
+    console.log(results);
+
+    message = finalMessage(results); // {'message': 'You Won', 'color': 'green'}
+    console.log(message);
+
+    rpsFrontEnd(yourChoice.id, botChoice, message);
 }
 
 function randToRpsInt() {
@@ -27,6 +32,48 @@ function decideWinner(yourChoice, computerChoice) {
 
     const yourScore = rpsDatabase[yourChoice][computerChoice];
     const computerScore = rpsDatabase[computerChoice][yourChoice];
-    console.log(yourScore);
-    console.log(computerScore);
+
+    return [yourScore, computerScore];
+}
+
+function finalMessage([yourScore, computerScore]) {
+    if (yourScore === 0) {
+        return { 'message': 'You Lost!', 'color': 'red' };
+    } else if (yourScore === 0.5) {
+        return { 'message': 'You Tied!', 'color': 'yellow' };
+    } else {
+        return { 'message': 'You Won!', 'color': 'green' };
+    }
+}
+
+// all images
+let rock = document.getElementById('rock');
+let paper = document.getElementById('paper');
+let scissors = document.getElementById('scissors');
+let container = document.getElementById('container');
+
+function rpsFrontEnd(humanImageChoice, botImageChoice, finalMessage) {
+    const imagesDatabe = {
+        'rock': rock.src,
+        'paper': paper.src,
+        'scissors': scissors.src
+    }
+
+    // let's remove all the images
+    rock.remove();
+    paper.remove();
+    scissors.remove();
+
+    // create div element
+    let humanDiv = document.createElement('div');
+    let botDiv = document.createElement('div');
+    let messageDiv = document.createElement('div');
+
+    humanDiv.innerHTML = `<img class='humanChoice' src='${imagesDatabe[humanImageChoice]}'>`;
+    messageDiv.innerHTML = `<h2 class='message' style='color: ${finalMessage['color']}'>${finalMessage['message']}</h2>`
+    botDiv.innerHTML = `<img class='botChoice' src='${imagesDatabe[botImageChoice]}'>`;
+
+    container.appendChild(humanDiv);
+    container.appendChild(messageDiv);
+    container.appendChild(botDiv);
 }
