307bee2160d5c8131f8b0e26f2bf66fb094cf242
MdTasib <ohidulalam442@gmail.com>
2021-06-02 11:58:55 +0600

    blackjack challenge start javascript part

---
+++ lessons/Js-crash-course-projects/project-4/assets/js/script.js
@@ -0,0 +1,12 @@
+// Blackjack challenge start here
+
+const blackjackGame = {
+    'you': { 'scoreSpan': '#your-blackjack-result', 'div': '#your-box', 'score': 0 },
+    'dealer': { 'scoreSpan': '#dealer-blackjack-result', 'div': '#dealer-box', 'score': 0 },
+}
+
+document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackHit);
+
+function blackjackHit() {
+
+}
