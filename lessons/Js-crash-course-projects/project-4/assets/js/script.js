2a544deff6b684ac7e90a4e0980ce210e89e4fd6
MdTasib <ohidulalam442@gmail.com>
2021-06-02 13:46:31 +0600

    show card in the box

--- lessons/Js-crash-course-projects/project-4/assets/js/script.js
+++ lessons/Js-crash-course-projects/project-4/assets/js/script.js
@@ -5,8 +5,31 @@ const blackjackGame = {
     'dealer': { 'scoreSpan': '#dealer-blackjack-result', 'div': '#dealer-box', 'score': 0 },
 }
 
+const YOU = blackjackGame['you'];
+const DEALER = blackjackGame['dealer'];
+
+// Audio sound
+const hitSound = new Audio('assets/sounds/hit.wav');
+
+// All blackjack buttons
 document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackHit);
+document.querySelector('#blackjack-deal-button').addEventListener('click', blackjackDeal);
 
 function blackjackHit() {
+    showCard(YOU);
+}
 
+function showCard(activePlayer) {
+    let cardImage = document.createElement('img');
+    cardImage.src = 'assets/images/q.png';
+    document.querySelector(activePlayer['div']).appendChild(cardImage);
+    hitSound.play();
+}
+
+function blackjackDeal() {
+    let yourImages = document.querySelector('#your-box').querySelectorAll('img');
+
+    for (let i = 0; i < yourImages.length; i++) {
+        yourImages[i].remove();
+    }
 }
